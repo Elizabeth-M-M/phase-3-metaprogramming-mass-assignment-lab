f07a8ef535b1beaf100512951e2b50812484774a
Elizabeth <mwendemuthusi6@gmail.com>
2023-02-14 17:08:27 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
   # your code here
+  
+  def initialize (attr)
+    attr.each_key do|key, val|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", attr[key])
+    end
+  end
 end
+# ella= {
+# name:"ella",
+# birthday:"today",
+# hair_color:"blonde",
+# eye_color:"brown",
+# height:186
+# }
+# p1= Person.new(ella)
+# puts p1.height
